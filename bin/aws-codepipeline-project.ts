bb3679feca663565af52a5469235f0596854689f
aLLUPS <73pawara@gmail.com>
2022-03-20 15:09:56 +0530

    Setup initial CDK project

--- bin/aws-codepipeline-project.ts
+++ bin/aws-codepipeline-project.ts
@@ -5,17 +5,13 @@ import { AwsCodepipelineProjectStack } from '../lib/aws-codepipeline-project-sta
 
 const app = new cdk.App();
 new AwsCodepipelineProjectStack(app, 'AwsCodepipelineProjectStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  // to create a new isntance of the class AwsCdkCodePipelineStack. This class is defined in lib directory
+  // it is this section wehere we can all the details of the cdk pipeline
+  
+  env: {
+    account: "205633759295",    // replace with your aws account ID
+    region: "ap-south-1",       // replace with your preferred region
+  },
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/aws-codepipeline-project-stack.ts
+++ lib/aws-codepipeline-project-stack.ts
@@ -1,16 +1,23 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
 
-export class AwsCodepipelineProjectStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class AwsCodepipelineProjectStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCodepipelineProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'CDKTestPipeline',       // Creating a new code pipeline which is a construct
+      synth: new ShellStep('Synth', {        // Add a new synthesis 'shellstep' which will be pointed at our gihub repository 
+        input: CodePipelineSource.gitHub('aLLUPS/aws-codepipeline-project', 'main'), // replace the GitHub repository name with 'user-name/repository-name'
+        
+        // The build steps for the pipeline are defined by these commands
+        
+        commands: ['npm ci',
+                   'npm run build',
+                   'npx cdk synth']
+      }),
+    })
   }
 }
